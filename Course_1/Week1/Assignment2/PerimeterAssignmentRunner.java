49e836083f728f31e07803887289b1cfc9d0ebd2
LalityaSawant <lalitya.sawant@gmail.com>
2018-04-25 01:15:03 -0700

    Mergered assigmnet 1 code here

--- Course_1/Week1/Assignment2/PerimeterAssignmentRunner.java
+++ Course_1/Week1/Assignment2/PerimeterAssignmentRunner.java
@@ -21,23 +21,68 @@ public class PerimeterAssignmentRunner {
     }
 
     public int getNumPoints (Shape s) {
-        // Put code here
-        return 0;
+        //Initiating numPoint to 0 which will count number of points
+        int numPoints = 0;
+        // read points from the Shape s
+        for (Point p : s.getPoints()) {        
+           numPoints++;         
+        }                   
+        // return the numPoints to display
+        return numPoints;
     }
 
     public double getAverageLength(Shape s) {
-        // Put code here
-        return 0.0;
+        double avgLength = 0;
+        int numPoints = getNumPoints(s);
+        double totalPerimeter = getPerimeter(s);        
+        avgLength = totalPerimeter / numPoints;
+        return avgLength;
     }
 
     public double getLargestSide(Shape s) {
-        // Put code here
-        return 0.0;
+        double largeSide = 0;
+        double currSide = 0;
+        double prevSide = 0;
+                
+        Point prevPt = s.getLastPoint();
+
+        for (Point currPt : s.getPoints()) {
+
+            currSide = prevPt.distance(currPt);
+            
+            if (currSide > prevSide ) {
+                  largeSide = currSide;
+                 } else
+                 {
+                  largeSide = prevSide;
+            }
+                prevSide = currSide;
+        }                
+        
+        return largeSide; 
     }
+    
 
     public double getLargestX(Shape s) {
-        // Put code here
-        return 0.0;
+        double largestX=0;
+        double currX = 0;
+        double prevX = 0;
+        
+        Point prevPt = s.getLastPoint();
+
+        for (Point currPt : s.getPoints()) {
+
+            currX = currPt.getX();
+            
+            if (currX > prevX ) {
+                  largestX = currX;
+                 } else
+                 {
+                  largestX = prevX;
+            }
+                prevX = currX;
+        }                
+        return largestX;
     }
 
     public double getLargestPerimeterMultipleFiles() {
@@ -90,6 +135,18 @@ public class PerimeterAssignmentRunner {
         Shape s = new Shape(fr);
         double length = getPerimeter(s);
         System.out.println("perimeter = " + length);
+        
+        int numPoints = getNumPoints(s);
+        System.out.println("no of Ponts in shape : " + numPoints);
+        
+        double avgLength = getAverageLength(s);
+        System.out.println("avg Length : " + avgLength);
+        
+        double largeSide = getLargestSide(s);
+        System.out.println("Large side is : " + largeSide);
+        
+        double largestX = getLargestX(s);
+        System.out.println("Largest X is : " + largestX);
     }
     
     public void testPerimeterMultipleFiles() {
