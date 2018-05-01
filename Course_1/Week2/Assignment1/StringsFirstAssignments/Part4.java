1aa79f0a00cad2eda00b95d88bfafbf30c53d561
LalityaSawant <lalitya.sawant@gmail.com>
2018-05-01 12:13:29 -0700

    Courseera Java course1 week 2 assignment 1 Part 4 Completed

--- Course_1/Week2/Assignment1/StringsFirstAssignments/Part4.java
+++ Course_1/Week2/Assignment1/StringsFirstAssignments/Part4.java
@@ -5,20 +5,30 @@
  * @author (your name) 
  * @version (a version number or a date)
  */
+
+import edu.duke.URLResource;
+
 public class Part4 {
   public String getYoutubeString() {
-      String ytString = "";
+      String ytString = "No youtube.com found";
       URLResource ur = new URLResource("http://www.dukelearntoprogram.com/course2/data/manylinks.html");
       for (String s : ur.words()) {
-          int ytInd = s.indexOf("youtube.com") ;          
+          int ytInd = s.toLowerCase().indexOf("youtube.com") ;          
             if ( ytInd != -1 ) {
                 int firstInd = s.indexOf("\"");
                 int scndInd = s.indexOf("\"",ytInd +1);
                     if (firstInd != -1 || scndInd !=-1) {
                          ytString = s.substring(firstInd,scndInd);
+                         return ytString;
                     }
             }     
       } 
      return ytString;  
   }
-}
+  
+  public void testGetYoutubeUrl() {
+      Part4 p4 = new Part4();
+      String result = p4.getYoutubeString();
+      System.out.println(result);
+  } 
+} 
